71e0727facac45a0b3776fb38ef29745c031467f
Cikgu Zamzila Mohamat <zam7924@gmail.com>
2026-09-22 14:55:30 +0800

    Add dedicated Simulasi UASA Google Sheet endpoint

---
+++ data-config.js
@@ -0,0 +1,2 @@
+// Google Apps Script Web App khas Simulasi UASA 2026.
+window.HISTORYVERSE_DATA_URL = 'https://script.google.com/macros/s/AKfycby31lsjgt7DAd3tvtNyVhYEq1KUI45pXuo3MCzQUS4ZbDg3N0QA2LfHXC-x7tvmmbGc/exec';
