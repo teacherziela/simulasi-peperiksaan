f7639d173671c1fb66a424b5970c1d85f0a40815
Cikgu Zamzila Mohamat <zam7924@gmail.com>
2026-09-22 14:41:02 +0800

    Remove unused Google data integration helper

--- data-config.js
+++
@@ -1,2 +0,0 @@
-// Google Apps Script Web App untuk simpan markah & refleksi Simulasi UASA.
-window.HISTORYVERSE_DATA_URL = 'https://script.google.com/macros/s/AKfycbza6VJTWBI5-UDO-nyEQtp243ZfyDMQ8XW1alRzPF3CxpkPVZ2zaYOTKtvna_B0h_KI7g/exec';
