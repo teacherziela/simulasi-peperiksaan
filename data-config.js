b7ec7c8119ba1f48f59f6e13e9707ab1e0f3e747
Cikgu Zamzila Mohamat <zam7924@gmail.com>
2026-09-22 14:39:18 +0800

    Activate Google Sheet storage for UASA simulation

--- data-config.js
+++ data-config.js
@@ -1,3 +1,2 @@
-// Masukkan URL Web App Google Apps Script selepas deploy.
-// Contoh: https://script.google.com/macros/s/XXXXXXXXXXXX/exec
-window.HISTORYVERSE_DATA_URL = '';
+// Google Apps Script Web App untuk simpan markah & refleksi Simulasi UASA.
+window.HISTORYVERSE_DATA_URL = 'https://script.google.com/macros/s/AKfycbza6VJTWBI5-UDO-nyEQtp243ZfyDMQ8XW1alRzPF3CxpkPVZ2zaYOTKtvna_B0h_KI7g/exec';
